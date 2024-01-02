3a54fda7c42d09bd11310d3fd0829f0706d6f6ce
Omarr-kh <omar1752003@gmail.com>
2024-01-02 03:17:18 -0800

    followed pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,19 +5,20 @@ import sys
 
 
 def main():
-    employee_id = int(sys.argv[1])
+    id = int(sys.argv[1])
     completed = []
     data = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 
     total = 0
     for employee in data:
-        if employee.get('userId') == employee_id:
+        if employee.get('userId') == id:
             if employee.get('completed'):
                 completed.append(employee['title'])
             total += 1
 
-    employee = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
-    print(f'Employee {employee.get("name")} is done with tasks({len(completed)}/{total}):')
+    employee = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}').json()
+    name = employee.get("name")
+    print(f'Employee {name} is done with tasks({len(completed)}/{total}):')
     for title in completed:
         print(f"\t {title}")
 
