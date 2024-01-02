cd9a4c5fe9e785d80815fd871dca078b4ed4c13a
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-01-02 21:18:31 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,29 +3,31 @@
 import json
 import requests
 
-
 if __name__ == '__main__':
-    url = f'https://jsonplaceholder.typicode.com/users/'
-    users = requests.get(url).json()
+    employees_url = 'https://jsonplaceholder.typicode.com/users/'
+    employees_response = requests.get(employees_url)
+    employee_list = employees_response.json()
 
-    users_dict = {}
+    all_tasks_json = {}
 
-    todos_url = 'https://jsonplaceholder.typicode.com/todos/'
-    todos = requests.get(url).json()
+    for employee in employee_list:
+        emp_id = employee.get('id')
+        emp_username = employee.get('username')
 
-    for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
+        tasks_url = 'https://jsonplaceholder.typicode.com/todos/'
+        tasks_response = requests.get(tasks_url)
+        all_tasks = tasks_response.json()
+        filtered_tasks = [
+            task for task in all_tasks if task.get('userId') == emp_id]
 
-        user_tasks = [task for task in todos if task.get('userId') == user_id]
+        formatted_tasks = []
+        for task in filtered_tasks:
+            task_info = {'username': emp_username, 'task': task.get('title'),
+                         'completed': task.get('completed')}
+            formatted_tasks.append(task_info)
 
-        tasks_json = []
-        for task in user_tasks:
-            entry = {'username': username, 'task': task.get('title'),
-                     'completed': task.get('completed')}
-            tasks_json.append(entry)
+        all_tasks_json[f'{emp_id}'] = formatted_tasks
 
-        users_dict[f'{user_id}'] = tasks_json
-
+    # Save the information to a JSON file:
     with open('todo_all_employees.json', 'w') as f:
-        json.dump(users_dict, f)
+        json.dump(all_tasks_json, f)
