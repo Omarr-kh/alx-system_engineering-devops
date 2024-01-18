2d9c86e8ddb47c9bbcef2723486e28b415e2268b
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-01-18 21:19:19 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" return top ten posts for a subreddit """
+''' return top ten posts for a subreddit '''
 import requests
 
 
 def top_ten(subreddit):
-    """ function to return top 10 posts """
+    ''' function to return top 10 posts '''
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {"User-Agent": "Mozilla/5.0 (by Omar)"}
+    headers = {'User-Agent': 'Mozilla/5.0 (by Omar)'}
     params = {'limit': 9}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json().get("data")
-        hot_posts = data.get("children")
+        data = response.json().get('data')
+        hot_posts = data.get('children')
 
         for post in hot_posts:
             print(post['data']['title'])
