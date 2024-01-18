432b974da898d8daef1b19fd5d89406427ed7898
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-01-18 20:54:39 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" return number of subscribers for a subreddit """
+"""Module to query the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function to return number of subscribers """
+    """Query the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {"User-Agent": "Mozilla/5.0 (Linux x86_64) Omar"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Youssef'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json().get("data")
-        num_subscribers = data.get("subscribers")
-
-        return num_subscribers
+        return response.json()['data']['subscribers']
     else:
         return 0
