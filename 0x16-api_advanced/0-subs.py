58edc534dbd76ce136b1443d0cc58e518623fd00
Omarr-kh <omar1752003@gmail.com>
2024-01-18 10:49:38 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ function to return number of subscribers """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    url_header = {"user-agent": "request"}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url_header = {"User-Agent": "Mozilla"}
     response = requests.get(url, headers=url_header, allow_redirects=False)
 
-    if response.status_code != 200:
-        return 0
-    data = response.json().get("data")
-    num_subscribers = data.get("subscribers")
+    if response.status_code == 200:
+        data = response.json().get("data")
+        num_subscribers = data.get("subscribers")
 
-    return num_subscribers
+        return num_subscribers
+    else:
+        return 0
